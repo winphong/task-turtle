d8555a61b8737a6e76a410af01bbcf0cc73c20ad
Tan Wei Jie <xwei_jiex@hotmail.com>
2018-10-09 14:56:09 +0800

    Update dbfunction

--- dbfunction.php
+++ dbfunction.php
@@ -3,20 +3,28 @@
     $dbType = "none";
 
     function getDbConnect() {
-        // get a database connection to postgres database
-        $con = pg_connect("host=localhost dbname=project user=postgres password=root port=5432");
+        try {
+            // get a database connection to postgres database
+            $con = pg_connect("host=localhost dbname=project user=postgres password=root port=5432");
 
-        // check if postgres database connection failed
-        if (!$con) {
-            $con = mysqli_connect("localhost:3306", "newuser01", "st2220", "tasksourcingdb"); // get connection to mysql
-
-            // if mysql database connection succeeds
+            // if postgres database connection succeeds
             if ($con) {
-                $dbType = "mysql";
+                $dbType = "postgres";
             }
-        } else {
-            $dbType = "postgres";
+        } catch (Exception $e) {
+            try {
+                // get a database connection to postgres database
+                $con = mysqli_connect("localhost:3306", "newuser01", "st2220", "tasksourcingdb"); // get connection to mysql
+
+                // if mysql database connection succeeds
+                if ($con) {
+                    $dbType = "mysql";
+                }
+            } catch (Exception $e) {
+                echo "No database connection established.";
+            }
         }
+
 
         return $con;
     }
