5a9c9ca6a6dc572c9693d4b2e5a6acf10017307f
Mingrui Tan <mingtui.tan@yahoo.com>
2018-10-09 19:58:06 +0800

    link taskList to taskPage

--- taskList.php
+++ taskList.php
@@ -7,10 +7,12 @@
 		echo 'Not connected to server';
 	}
 
-	$retrieveTask = pg_query("SELECT title, description, task_date, creator FROM task ORDER BY post_date DESC");
+	$retrieveTask = pg_query("SELECT taskid, title, description, task_date, creator FROM task ORDER BY post_date DESC");
 
 	while ( $arr = pg_fetch_array( $retrieveTask, NULL, PGSQL_ASSOC ) ) {
 
-            echo "</br>".'<div style="border:1px solid; padding:20px; margin-bottom:20px;">'.$arr['title']."</br>".$arr['description']."</br>".$arr['task_date']."</br>".$arr['creator'].'</div>';
+		$taskid = $arr['taskid'];
+
+        echo "</br>".'<div style="border:1px solid; padding:20px; margin-bottom:20px;">'."Task title: ".$arr['title']."</br>"."Description: ".$arr['description']."</br>"."Date: ".$arr['task_date']."</br>"."Creator: ".$arr['creator'].'<form action="taskPage.php" method="POST"><input type="hidden" name="taskid" value='.$taskid.'><input type="submit" value="Go to task"></form></div>';
     }
 ?>
