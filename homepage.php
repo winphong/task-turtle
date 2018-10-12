1a61930f36814b78e3d0bfd7ead7a9158bc5b9a1
nattanyz <natalietan8@gmail.com>
2018-10-12 12:49:21 +0800

    added css styles and restructured html

--- homepage.php
+++ homepage.php
@@ -1,24 +1,29 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title> Task Turtle </title>
-    </head>
+  <head>
+      <title> TaskTurtle | Home </title>
+      <link rel="stylesheet" type="text/css" href="styles.css">
+      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+  </head>
 
-    <body>
-    	<h1> Welcome to task turtle </h1>
-
-    	<div class="signup">
-
-    		<h3> Login to browse </h3>
-		
-			<form action="loginPage.html" method="post">
-    			<input type="submit" name="signin" value="Log in" />
-    		</form>
-    	
-	    	<form action="signupPage.html" method="post">	
-	    		<input type="submit" name="signup" value="Sign up for an account" />
-	    	</form>
-		<div>
-
-    </body> 
+  <body>
+    <div id="header">
+      <div id="branding"> <!-- contains sitename and logo to align left -->
+        <img id="logo" src="assets/logo.png"></img>
+        <p id="sitename"><a href="homepage.php">TaskTurtle</a></p>
+      </div>
+      <ul class="nav" id="topnav">
+        <li><a href="/loginPage.html">Log In</a></li>
+        <li><a href="/signupPage.html">Sign Up</a></li>
+      </ul>
+    </div> <!-- end header -->
+    <main class="flex" id="index-main">
+  	  <h1 class="welcome"> Welcome to TaskTurtle! </h1>
+      <h3 class="tagline"> Login to browse our tasks or create your own! </h3>
+    	<ul class="nav" id="signup">
+        <li class="special-button"><a href="/loginPage.html">Log In</a></li>
+        <li class="special-button"><a href="/signupPage.html">Sign Up</a></li>
+      </ul>
+	   </main>
+  </body>
 </html>
