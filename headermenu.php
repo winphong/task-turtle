d99a3e6ac29f1e276423cce84376d2291d56ca9b
nattanyz <natalietan8@gmail.com>
2018-10-12 15:11:43 +0800

    modified headermenu.php, cosmetic changes
    made viewProfileSettings a little prettier

--- headermenu.php
+++ headermenu.php
@@ -1,7 +1,3 @@
-<div style="float: right;">
-    <?php
-        echo "<h3 style='display: inline-block; padding: auto 20px;'>Welcome, " . $userinfo['display_name'] . "</h3>";
-    ?>
-    <button style="display: inline-block;" onclick="location.href='viewProfileSettings.php';">Settings</button>
-    <button style="display: inline-block;" onclick="location.href='logout.php';">Logout</button><br />
-</div>
+<li class="user-welcome">Welcome, <?php echo $userinfo['display_name'] ?>!</li>
+<li onclick="location.href='viewProfileSettings.php';">Settings</li>
+<li onclick="location.href='logout.php';">Logout</li>
