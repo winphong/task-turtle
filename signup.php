0b5f3b2e103ec65bc2ec9f601cb3267a054dd50c
Mingrui Tan <mingtui.tan@yahoo.com>
2018-11-05 02:52:13 +0800

    fix the mix-up of display name and user profile during sign up

--- signup.php
+++ signup.php
@@ -14,10 +14,10 @@
 
 		$username = $_POST['username'];
 		$password = $_POST['password'];
-		$name = ($_POST['userProfile'] == "") ? $username : "'" . $_POST['userProfile'] . "'";
-		$userProfile = ($_POST['userProfile'] == "") ? "DEFAULT" : "'" . $_POST['userProfile'] . "'";
+		$name = $_POST['name'];
+		$userProfile = ($_POST['userProfile'] == "") ? "DEFAULT" : $_POST['userProfile'];
 
-        $queryStr = "INSERT INTO userTable VALUES ('$username', '$password', '$name', $userProfile)";
+        $queryStr = "INSERT INTO userTable VALUES ('$username', '$password', '$name', '$userProfile')";
 		$userSignUp = dbQuery($con, $queryStr);
 
 		if ($userSignUp) {
