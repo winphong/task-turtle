0ee737231878ff7a5a47706c1acc1e0faa870cc6
Tan Wei Jie <xwei_jiex@hotmail.com>
2018-11-08 14:41:45 +0800

    Add function to get defined error messages Change select bid assignment conditions to print error messages

--- selectBid.php
+++ selectBid.php
@@ -7,29 +7,28 @@
 
 	if (!$con) {
 		echo 'Not connected to server';
-	}
-
-	$winningBidder = $_POST['winningBidder'];
-
-	session_start();
-	$taskid = $_SESSION['taskid'];
-
-	$failedQueryStr = "UPDATE bid SET status='failed' WHERE task='$taskid' AND bidder!='$winningBidder'";
-	$updateFailedStatus = dbQuery($con, $failedQueryStr);
-
-	$successQueryStr = "UPDATE bid SET status='successful' WHERE task='$taskid' AND bidder='$winningBidder'";
-	$updateSuccessStatus = dbQuery($con, $successQueryStr);
-
-	$assignQueryStr = "INSERT INTO assigned_to VALUES ('$taskid', '$winningBidder', 'FALSE')";
-	$assignTask = dbQuery($con, $assignQueryStr);
-
-	if ( $updateFailedStatus AND $updateSuccessStatus AND $assignQueryStr ) {
-		echo "Successfully assigned task to ".$winningBidder;
-		header("refresh=1; url = 'loggedInHomepage.html'");
 	} else {
-		echo "Assign failed";
-		header("refresh=0; url = 'selectBid.php'");
-	}
+	    $winningBidder = $_POST['winningBidder'];
 
+	    session_start();
+	    $taskid = $_SESSION['taskid'];
+
+	    $failedQueryStr = "UPDATE bid SET status='failed' WHERE task='$taskid' AND bidder!='$winningBidder'";
+
+	    $successQueryStr = "UPDATE bid SET status='successful' WHERE task='$taskid' AND bidder='$winningBidder'";
+
+	    $assignQueryStr = "INSERT INTO assigned_to VALUES ('$taskid', '$winningBidder', 'FALSE')";
+	    $assignTask = dbQuery($con, $assignQueryStr);
+
+	    if ( dbAffectedRows($con, $assignTask) > 0 ) {
+	        dbQuery($con, $failedQueryStr);
+	        dbQuery($con, $successQueryStr);
+		    echo "Successfully assigned task to " . $winningBidder . ".";
+		    header("refresh:1; url = 'loggedInHomepage.html'");
+	    } else {
+		    echo "Assign failed. " . dbGetErrorMessage($con);
+		    header("refresh:2; url = 'taskPage.php'");
+	    }
+    }
 
 ?>
