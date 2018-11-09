1831be192b0aea2c7cc1cbacadeacd08e686e8ee
Tan Wei Jie <xwei_jiex@hotmail.com>
2018-11-09 11:11:06 +0800

    Update task creation handler to show messages correctly

--- handleTaskCreation.php
+++ handleTaskCreation.php
@@ -8,43 +8,42 @@
     $con = getDbConnect();
 
     if (!$con) {
-
         echo 'Not connected to server';
-    }
+    } else {
 
-    if (isset($_POST['create_task'])) {
+        if (isset($_POST['create_task'])) {
 
-        $title = $_POST['title'];
-        $description = ($_POST['description'] == "") ? "DEFAULT" : "'" . $_POST['description'] . "'";
-        $task_date = $_POST['task_date'];
-        $start_time = $_POST['start_time'];
-        $end_time = $_POST['end_time'];
-        $location = $_POST['location'];
-        $category = $_POST['category'];
-        $post_date = date("Y-m-d");
-        $username = isset($_POST['creator']) ? $_POST['creator'] : $_SESSION['username'];
+            $title = $_POST['title'];
+            $description = ($_POST['description'] == "") ? "DEFAULT" : "'" . $_POST['description'] . "'";
+            $task_date = $_POST['task_date'];
+            $start_time = $_POST['start_time'];
+            $end_time = $_POST['end_time'];
+            $location = $_POST['location'];
+            $category = $_POST['category'];
+            $post_date = date("Y-m-d");
+            $username = isset($_POST['creator']) ? $_POST['creator'] : $_SESSION['username'];
 
-        $queryStr = "INSERT INTO task(title, description, task_date, start_time, end_time, location, category, post_date, creator) VALUES ('$title', $description, '$task_date', '$start_time', '$end_time', '$location', '$category', '$post_date', '$username')";
-        $userCreateTask = dbQuery($con, $queryStr);
+            $queryStr = "INSERT INTO task(title, description, task_date, start_time, end_time, location, category, post_date, creator) VALUES ('$title', $description, '$task_date', '$start_time', '$end_time', '$location', '$category', '$post_date', '$username')";
+            $userCreateTask = dbQuery($con, $queryStr);
 
-        if ($userCreateTask) {
-            echo "Task created successfully!";
-            
-            if (isset($_POST['creator'])) {
-                
-                header("refresh:0;url = 'adminSearchResultPage.html'");
-            
+            if (dbAffectedRows($con, $userCreateTask) > 0) {
+                echo "Task created successfully!";
+
+                if (isset($_POST['creator'])) {
+
+                    header("refresh:0.5;url = 'adminSearchResultPage.html'");
+
+                } else {
+
+                    header("refresh:0.5; url = 'loggedInHomepage.html'");
+                }
+
             } else {
-
-                header("refresh:0; url = 'loggedInHomepage.html'");
+                echo "Task creation failed. " . dbGetErrorMessage($con);
+                header("refresh:3; url = createTask.php");
             }
 
-        } else {
-            echo "Task creation failed.";
-            header("refresh:0; url = createTask.php");
         }
 
-
     }
-
 ?>
